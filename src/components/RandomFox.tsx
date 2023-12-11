ec5233af030379516d970d654cc9e9a88b081829
Xavi Flores <xa_franc@live.com>
2023-12-10 20:46:42 -0600

    finish

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,14 +1,52 @@
+
 // import type { FunctionComponent, FC } from "react"
+
+import { useRef, useEffect, useState } from "react";
+import type { ImgHTMLAttributes } from "react";
 
 // export const RandomFox = () => {
 //     return <img></img>
 // }
-export const RandomFox = (): JSX.Element => {
-    const random = () => Math.floor(Math.random() * 123) + 1;
 
-    const image: string = `https://randomfox.ca/images/${random()}.jpg`
+type ImageNative = ImgHTMLAttributes<HTMLImageElement>
 
-    return <img width={320} height='auto' className='rounded' src={image} />
+type LazyImagesProps = {
+    src: string,
+};
+
+type Props = LazyImagesProps & ImageNative;
+
+export const LazyImage = ({ src, ...ImgProps }: Props): JSX.Element => {
+
+    // const image: string = `https://randomfox.ca/images/${random()}.jpg`
+    const node = useRef<HTMLImageElement>(null);
+    const [currentSrc, setCurrentSrc] = useState("");
+
+    useEffect(() => {
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry => {
+                if (entry.isIntersecting) {
+                    setCurrentSrc(src);
+                }
+            }))
+        })
+
+
+        // console.log(node.current)
+
+
+
+
+        observer.observe(node.current!)
+
+        return () => {
+            observer.disconnect();
+        }
+
+    }, [src]);
+
+    return <img ref={node} src={currentSrc} {...ImgProps} />
 }
 // export const RandomFox: FunctionComponent = () => {
 //     return <img></img>
